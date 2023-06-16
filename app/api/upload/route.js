f1d477d3189e432f32f2f86751ae3f3b88844164
PauliusJaras <38277493+PauliusJaras@users.noreply.github.com>
2023-06-17 02:39:32 +0300

    image upload to S3

---
+++ app/api/upload/route.js
@@ -0,0 +1,63 @@
+import { NextResponse } from "next/server";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+const bucketName = "paul-next-ecommerce";
+
+export const config = { runtime: "experimental-edge" };
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const file = formData.get("file") || null;
+
+  if (!file) {
+    return NextResponse.json(
+      { error: "File blob is required." },
+      { status: 400 }
+    );
+  }
+
+  //reading file
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  //connection to s3
+  const client = new S3Client({
+    region: "eu-north-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const links = [];
+
+  try {
+    const ext = file.type.split("/").pop();
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const filename = `${file.name.replace(
+      /\.[^/.]+$/,
+      ""
+    )}-${uniqueSuffix}.${ext}`;
+
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: filename,
+        Body: buffer,
+        ACL: "public-read",
+        ContentType: ext,
+      })
+    );
+
+    const link = `https://${bucketName}.s3.amazonaws.com/${filename}`;
+    links.push(link);
+
+  } catch (e) {
+    console.error("Error while trying to upload a file\n", e);
+    return NextResponse.json(
+      { error: "Something went wrong." },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ links });
+}
--- app/components/ProductForm.jsx
+++ app/components/ProductForm.jsx
@@ -7,12 +7,27 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
+
+  async function uploadImages(event) {
+    const files = event.target?.files;
+    console.log(files);
+    if (files?.length > 0) {
+      const formData = new FormData();
+      for (const file of files) {
+        formData.append("file", file, file.name);
+      }
+      const response = await axios.post("/api/upload", formData);
+      setImages((oldImages) => [...oldImages, ...response.data.links]);
+    }
+  }
 
   async function saveProduct(event) {
     event.preventDefault();
@@ -23,7 +38,7 @@ export default function ProductForm({
     };
     if (_id) {
       //update
-      await axios.put("/api/products", {_id, ...productData});
+      await axios.put("/api/products", { _id, ...productData });
     } else {
       //create
       await axios.post("/api/products", productData);
@@ -45,6 +60,42 @@ export default function ProductForm({
           value={title}
           onChange={(event) => setTitle(event.target.value)}
         />
+        <label>Images</label>
+        <div className="mb-2 flex flex-wrap gap-2">
+          {!!images?.length &&
+            images.map((link, index) => (
+              <div className="h-24" key={index}>
+                <img className="rounded-lg" src={link} alt="img" />
+              </div>
+            ))}
+          <label
+            className="w-24 h-24 flex items-center 
+          justify-center text-sm gap-1 text-gray-500 rounded-md
+          bg-gray-200"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+              />
+            </svg>
+            <span>Upload</span>
+            <input
+              type="file"
+              className="hidden"
+              onChange={uploadImages}
+            ></input>
+          </label>
+          {!images?.length && <div>This product has no images</div>}
+        </div>
         <label>Description</label>
         <textarea
           placeholder="Description"
